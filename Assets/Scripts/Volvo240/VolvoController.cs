e18c0c95d17cab324e664f0447feeb0b4773c642
Sam Nilsson <sam64114208@gmail.com>
2022-10-22 16:31:32 +0200

    Better car

--- Assets/Scripts/Volvo240/VolvoController.cs
+++ Assets/Scripts/Volvo240/VolvoController.cs
@@ -3,24 +3,46 @@ using UnityEngine.InputSystem;
 
 public class VolvoController : MonoBehaviour
 {
-    [SerializeField] private float Acceleration = 25f;
+    [SerializeField] private float acceleration = 25f;
+    [SerializeField] private float maxSpeed = 6.66f;
     private Rigidbody rb;
 
     private Vector2 moveDirection;
     private bool moving;
+    private float targetYaw;
 
     private void OnMove(InputValue input)
     {
         moveDirection = input.Get<Vector2>();
         moving = moveDirection.x != 0 || moveDirection.y != 0;
+        targetYaw = Mathf.Acos(moveDirection.x) * Mathf.Rad2Deg;
+        if (moveDirection.y > 0)
+            targetYaw = 360f - targetYaw;
+    }
+
+    private void Drive()
+    {
+        if (moving)
+        {
+            float deltaSpeed = Time.deltaTime * acceleration;
+            Vector3 moveDirection3d = new(moveDirection.x, 0, moveDirection.y);
+
+            if(rb.velocity.magnitude >= maxSpeed * maxSpeed)
+            {
+                float newDirDiff = Vector3.Dot(rb.velocity.normalized, moveDirection3d);
+                float reducePrecent = Mathf.Clamp01(1 + newDirDiff);
+                rb.velocity -= rb.velocity.normalized * (deltaSpeed * reducePrecent);
+            }
+
+            rb.velocity += moveDirection3d * (Time.deltaTime * acceleration);
+
+            transform.rotation = Quaternion.Euler(0, targetYaw, 0);
+        }
     }
 
     private void Update()
     {
-        if (moving)
-        {
-            rb.velocity += new Vector3(moveDirection.x, 0, moveDirection.y) * (Time.deltaTime * Acceleration);
-        }
+        Drive();
     }
 
     private void Awake()
