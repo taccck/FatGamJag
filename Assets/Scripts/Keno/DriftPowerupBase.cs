3736bba105c8bb4dc79970229fd205f23052d870
Alexander Dolk <alexander@dolken.net>
2022-10-22 16:07:35 +0200

    Added Content folder and powerups

---
+++ Assets/Scripts/Keno/DriftPowerupBase.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Drift Powerup", menuName = "Gameplay/Powerups/Drift Powerup")]
+public class DriftPowerupBase : PowerupBase
+{
+	public override void ApplyPowerup()
+	{
+		//Apply Drift powerup
+	}
+}
---
+++ Assets/Scripts/Keno/StrengthPowerupBase.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Strength Powerup", menuName = "Gameplay/Powerups/Strength Powerup")]
+public class StrengthPowerupBase : PowerupBase
+{
+	public override void ApplyPowerup()
+	{
+		//Apply strength
+	}
+}
