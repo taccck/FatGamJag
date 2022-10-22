f12afe4cb05022b99decdce281e4e449a938ec07
Alvin Jangvik <alvin.jangvik@futuregames.nu>
2022-10-23 01:46:32 +0200

    less damage during high speed

--- Assets/Scripts/Volvo240/VolvoFighting.cs
+++ Assets/Scripts/Volvo240/VolvoFighting.cs
@@ -24,7 +24,12 @@ public class VolvoFighting : MonoBehaviour
 
     public void TakeDamage(float dealtDamage)
     {
-        health -= dealtDamage;
+        // 1f is from frustration, find good value tomorrow. - Alvin, 1:45
+        float modifiedDamage = dealtDamage - (dealtDamage * (rb.velocity.magnitude / (VolvoConfig.Get.baseMaxSpeed * 1f)));
+        // Done here instead directly after "health -=" because of debug, feel free to change
+        modifiedDamage = modifiedDamage > 0 ? modifiedDamage : 0;
+        health -= modifiedDamage;
+        print("Damage: " + modifiedDamage);
         if(health <= 0)
         {
             print("is ded");
