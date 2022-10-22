725983be6817aa01d44da098630412f9e9bd1e2d
Alvin Jangvik <alvin.jangvik@futuregames.nu>
2022-10-22 16:14:08 +0200

    Alvin test scene, ignore userSettings

---
+++ Assets/Scripts/Enemies/EnemyBase.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class EnemyBase : MonoBehaviour
+{
+    [SerializeField] private GameObject Player;
+    [SerializeField] private float Health = 10;
+    [SerializeField] private float Attack = 1;
+    [SerializeField] private float Speed = 5;
+
+    private Rigidbody body;
+
+    private void Start()
+    {
+        body = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        moveTowardsPlayer();
+    }
+
+    private void moveTowardsPlayer()
+    {
+        Vector3 directionToPlayer = Player.transform.position - transform.position;
+        
+        body.AddForce(directionToPlayer * Speed, ForceMode.Force);
+    }
+}
