ec653e4c6f7a42de6fa151ae9e0a546c92aa50a1
Sam Nilsson <sam64114208@gmail.com>
2022-10-22 18:12:44 +0200

    Better turning

--- Assets/Scripts/Volvo240/VolvoController.cs
+++ Assets/Scripts/Volvo240/VolvoController.cs
@@ -5,6 +5,8 @@ public class VolvoController : MonoBehaviour
 {
     [SerializeField] private float acceleration = 25f;
     [SerializeField] private float maxSpeed = 6.66f;
+    [SerializeField] private float turnSpeed = 1f;
+    [SerializeField] private float driftTurnSpeed = 5f;
     private Rigidbody rb;
 
     private Vector2 moveDirection;
@@ -28,13 +30,13 @@ public class VolvoController : MonoBehaviour
     }
 
     private void Drive()
-    {        
+    {
         if (moving && !drifting)
         {
             float deltaSpeed = Time.deltaTime * acceleration;
             Vector3 moveDirection3d = new(moveDirection.x, 0, moveDirection.y);
 
-            if(rb.velocity.magnitude >= maxSpeed * maxSpeed)
+            if (rb.velocity.magnitude >= maxSpeed * maxSpeed)
             {
                 float newDirDiff = Vector3.Dot(rb.velocity.normalized, moveDirection3d);
                 float reducePrecent = Mathf.Clamp01(1 + newDirDiff);
@@ -49,14 +51,26 @@ public class VolvoController : MonoBehaviour
     {
         if (moving)
         {
-            transform.rotation = Quaternion.Euler(0, targetYaw, 0);
+            Vector3 angles = transform.rotation.eulerAngles;
+            float turnSpeedGraphed = Mathf.Sqrt(rb.velocity.magnitude * turnSpeed);
+            float deltaYaw = drifting ? driftTurnSpeed + turnSpeedGraphed : turnSpeedGraphed;
+            float distToTurn = targetYaw - angles.y;
+            int turnDirection = (int)Mathf.Sign(distToTurn);
+            if(Mathf.Abs(distToTurn) > 180)
+            {
+                distToTurn = Mathf.Abs(distToTurn) - 180;
+                turnDirection *= -1;
+            }
+            float percentToTurn = Mathf.Abs(distToTurn) / 180f;
+            float newYaw = angles.y + turnDirection * deltaYaw * percentToTurn;
+            transform.rotation = Quaternion.Euler(angles.x, newYaw, angles.z);
         }
     }
 
     private void Update()
     {
-        Turn();
         Drive();
+        Turn();
     }
 
     private void Awake()
