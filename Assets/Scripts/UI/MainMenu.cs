38ec5b6bc17c0812f15c4c0ef0b78a62692ae80c
Alvin Jangvik <alvin.jangvik@futuregames.nu>
2022-10-23 16:15:52 +0200

    main menu stuff

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -5,7 +5,7 @@ public class MainMenu : MonoBehaviour
 {
     public void StartGame()
     {
-        SceneManager.LoadScene("SamTest");
+        SceneManager.LoadScene("AlvinTestScene");
     }
 
     public void QuitGame()
